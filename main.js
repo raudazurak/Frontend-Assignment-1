da7b2102737b760d30d31ad6f3b106734d1b3d3a
raudazurak <99649820+raudazurak@users.noreply.github.com>
2023-03-15 20:20:39 +0100

    adding filters

--- main.js
+++ main.js
@@ -7,8 +7,25 @@ import 'bootstrap'
 let books = await (await fetch('./books.json')).json()
 
 displayBooks()
+categoryFilters()
+
+function categoryFilters() {
+    document.querySelector('.filters').innerHTML = `
+      <label><span>Filter by hobbies:</span>
+        <select class="hobbyFilter">
+          <option>all</option>
+          <option>UX</option>
+          <option>CSS</option>
+          <option>HTML</option> 
+        </select>
+      </label>
+    `;
+
+}
 
 //display all
+
+//making the filters
 
 //
 
@@ -16,9 +33,13 @@ function displayBooks(){
 
 let htmlArray = books.map(({title, author,price})=> `
 <div class="book">
+<img class="book-img" src="https://source.unsplash.com/random/?wallpaper">
+<div class="disc">
 <h3>${title}</h3>
 <p>${author}</p>
 <p>${price} kr</p>
+</div>
+<button class="buy-btn">buy</button>
 </div>
 `);
 
