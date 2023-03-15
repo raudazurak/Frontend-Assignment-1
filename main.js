651d0ec06e2a4a9665d8539454b14466b5d84956
raudazurak <99649820+raudazurak@users.noreply.github.com>
2023-03-15 18:17:47 +0100

    new changes

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 import './style.css'
 
+import 'bootstrap'
+
+//Fetch from json
+
+let books = await (await fetch('./books.json')).json()
+
+displayBooks()
+
+//display all
+
+//
+
+function displayBooks(){
+
+let htmlArray = books.map(({title, author,price})=> `
+<div class="book">
+<h3>${title}</h3>
+<p>${author}</p>
+<p>${price} kr</p>
+</div>
+`);
+
+document.querySelector('.bigs').innerHTML=htmlArray.join('');
+}
+
