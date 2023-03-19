6f2678d27392ab9cbd595fdcf7a768c6af02d1f6
raudazurak <99649820+raudazurak@users.noreply.github.com>
2023-03-19 21:23:17 +0100

    sorting options and shopping cart

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ let
   chosencategoryFilter = 'all',
   chosenSortOption,  
   ratioChoice,
-  ratio,
+  total = 0,
   view,
-  categories = [];
+  htmlArr,
+  categories = [],
+  cart = [];
   
 
 let books = await (await fetch('./books.json')).json()
@@ -19,6 +21,7 @@ getCategories()
 categoryFilters()
 sortingOptions()
 settingLevel()
+shoppingCart()
 displayBooks()
 
 function getCategories() {
@@ -51,9 +54,9 @@ function sortingOptions() {
     document.querySelector('.sortOptions').innerHTML = `
       <label><span>sort by</span>
         <select class="sortOption">
-          <option>Title</option>
-          <option>Price</option>
-          <option>Author</option>
+          <option>title</option>
+          <option>price</option>
+          <option>author</option>
         </select>
       </label>
       
@@ -71,6 +74,79 @@ function settingLevel() {
         </label>
     `}
 
+function shoppingCart() {
+
+
+document.querySelector('.shop').innerHTML = `
+<p>Shopping Cart</p>
+<div class="items"></div>
+<div class="final"></div>
+
+`
+document.querySelector('.shop').addEventListener(
+  'click',
+  event => {
+    if(cart.length === 0){
+      document.querySelector('.items').innerHTML = `
+      <hr/>
+      <p>Your cart is empty</p>
+      <hr/>
+      `
+      document.querySelector('.final').innerHTML = `
+      <p>Total: ${total}kr</p>
+      `
+    }else{
+       
+      htmlArr = cart.map(({
+      titleP, priceP
+      }) => `
+        <p>${titleP} ${priceP} 1</p>
+        <hr/>      
+      `)
+      document.querySelector('.items').innerHTML = htmlArr.join('')
+      document.querySelector('.final').innerHTML = `
+      <p>Total: ${total}kr</p>
+      `
+    }})
+
+    
+  
+    
+       
+
+    
+
+  
+}
+
+
+function sortedPrice(ratioChoice){
+  if(ratioChoice === 'ascending'){
+  books.sort(({ price: aPrice }, { price: bPrice }) =>
+  aPrice > bPrice ? 1 : -1)
+  console.log(books)
+  return books
+  }else if(ratioChoice === 'descending'){
+  books.sort(({ price: aPrice }, { price: bPrice }) =>
+  aPrice > bPrice ? -1 : 1)
+  console.log(books)
+  return books
+}}
+
+function sortedTitle(ratioChoice){
+  if(ratioChoice === 'ascending'){
+  books.sort(({ title: aTitle }, { title: bTitle }) =>
+  aTitle > bTitle ? 1 : -1)
+  console.log(books)
+  return books
+  }else if(ratioChoice === 'descending'){
+  books.sort(({ title: aTitle }, { title: bTitle }) =>
+  aTitle > bTitle ? -1 : 1)
+  console.log(books)
+  return books
+}
+
+}
 
  function filterEffect(){
     if(chosencategoryFilter === 'all'){
@@ -78,20 +154,27 @@ function settingLevel() {
     }else if(chosencategoryFilter === 'UX' || chosencategoryFilter === 'CSS'
     || chosencategoryFilter === 'HTML' || chosencategoryFilter === 'JavaScript'
     ){
-        console.log(books.filter(({category}) => chosencategoryFilter === category))
+      books = books.filter(({category}) => chosencategoryFilter === category)
+        
         document.querySelector('.sortOption').addEventListener(
           'change',
           event => {
             chosenSortOption = event.target.value;
           console.log(chosenSortOption)
           
+          //take it to its own function and sort
         })
         document.querySelector('.level').addEventListener(
           'change',
           event => {
             ratioChoice = event.target.value;
+            if(chosenSortOption === 'price'){books = sortedPrice(ratioChoice)}
+           else if(chosenSortOption === 'title'){books = sortedTitle(ratioChoice)}
+           else if(chosenSortOption === 'author'){books = sortedAuthor(ratioChoice)}
           console.log(ratioChoice)
           
+          displayBooks()
+
         })
         
     }
@@ -135,13 +218,43 @@ document.querySelector('.disc').addEventListener(
       document.querySelector('.info').style.display = "block"
     }else{
       document.querySelector('.info').style.display = "none"
-    }
+    }})
 
-})
 
-document.querySelector('.disc').addEventListener(
+document.querySelector('.buy-btn').addEventListener(
   'click',
   event => {
+    var addTitle = document.querySelector('h3').innerText
+    var twoValues = document.querySelectorAll('.disc p')
+    var addAuthor = twoValues[0].innerText
+    var addPrice = twoValues[1].innerText
+    addPrice = addPrice.split(' ')
+    var p = addPrice[0]
 
-})}
+    
+    
+
+    let purchase = {
+      titleP: addTitle,
+      authorP: addAuthor,
+      priceP: Number(p)
+    }
+
+    cart.push(purchase)
+    console.log(purchase.priceP)
+    total += purchase.priceP
+    console.log(total)
+
+    console.log(cart)
+
+  })
+
+  /*for (let index = 0; index < cart.length; index++) {
+    let element = cart[index].priceP;
+    total += element
+    console.log(total)
+  }*/
+}
+
+
 
